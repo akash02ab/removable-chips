a8548f8f43d1b106e3ee641c9b4b7c4e0a7c8729
akash02ab <akash02.ab@gmail.com>
2021-06-23 09:12:06 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const input = document.getElementById("input");
+const add = document.getElementById("add");
+const chips = document.querySelector(".chips");
+
+let id = 0;
+
+const deleteChip = (thisid) => {
+    let allChips = document.querySelectorAll(".chip");
+    
+    for(let chip of allChips) {
+        if(chip.id == thisid) {
+            chips.removeChild(chip);
+            break;
+        }
+    }
+}
+
+const addChip = (id) => {
+    let text = input.value;
+
+    let chip = document.createElement("div");
+    let avatar = document.createElement("img");
+    let name = document.createElement("p");
+    let close = document.createElement("span");
+
+    avatar.src = "./avatar.png";
+    name.innerText = text;
+    close.innerText = "X";
+
+    close.addEventListener("click", () => deleteChip(id));
+
+    chip.appendChild(avatar);
+    chip.appendChild(name);
+    chip.appendChild(close);
+
+    chip.id = id;
+    chip.classList.add("chip");
+
+    chips.appendChild(chip);
+}
+
+add.addEventListener("click", () => {addChip(id); id += 1;});
